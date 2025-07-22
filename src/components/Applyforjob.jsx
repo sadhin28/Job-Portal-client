2fbcdeec6706cf53b97eaf835990ca813722f63a
sadhin28 <taosifsadhin113@gmail.com>
2025-07-22 21:30:11 +0600

    add apply for a job page

---
+++ src/components/Applyforjob.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Applyforjob = () => {
+    return (
+        <div>
+            <h1>Apply For A Job</h1>
+        </div>
+    );
+};
+
+export default Applyforjob;
