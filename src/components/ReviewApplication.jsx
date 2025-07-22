2de972705f6e49a5e62bc1b81ea1d23be9893cc6
sadhin28 <taosifsadhin113@gmail.com>
2025-07-22 21:33:16 +0600

    add review application page

---
+++ src/components/ReviewApplication.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReviewApplication = () => {
+    return (
+        <div>
+            <h1>Review application</h1>
+        </div>
+    );
+};
+
+export default ReviewApplication;
