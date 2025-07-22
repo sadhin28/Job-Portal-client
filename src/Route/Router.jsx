f38862063a3486a7acd664e2b043e3bfbad13cc3
sadhin28 <taosifsadhin113@gmail.com>
2025-07-22 21:28:44 +0600

    create job portal

---
+++ src/Route/Router.jsx
@@ -0,0 +1,20 @@
+import {
+  createBrowserRouter,
+} from "react-router-dom";
+import Root from "../Root/Root";
+import Home from "../components/Home";
+
+const router = createBrowserRouter([
+   {
+    path:'/',
+    element:<Root></Root>,
+    children:[
+        {
+            path:'/',
+            element:<Home></Home>
+        }
+    ]
+   }
+])
+
+export default router;
---
+++ src/components/Alljobs.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Alljobs = () => {
+    return (
+        <div>
+            <h1>All Jobs</h1>
+        </div>
+    );
+};
+
+export default Alljobs;
