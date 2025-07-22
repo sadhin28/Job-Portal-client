7d866543801c992c097c479b498bba601a4eb176
sadhin28 <taosifsadhin113@gmail.com>
2025-07-22 23:01:12 +0600

    active link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../Provider/Authprovider";
 
 const Navbar = () => {
     const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -8,8 +9,7 @@ const Navbar = () => {
     const toggleSidebar = () => {
         setSidebarOpen(!sidebarOpen);
     };
-    const [user,setuser]=useState(true);
-    
+    const { user } = useContext(AuthContext)
     const navlink = [
         {
             name: 'Home',
@@ -19,7 +19,7 @@ const Navbar = () => {
             name: 'All Jobs',
             link: '/jobs'
         },
-        {
+        ...(user ? [{
             name: 'Add Job',
             link: '/add-jobs'
         },
@@ -30,20 +30,22 @@ const Navbar = () => {
         {
             name: 'My Job Posts',
             link: '/my-jobs'
-        },
+        },] : [])
+
     ]
     return (
         <>
             {/* Navbar */}
-            <nav className="bg-blue-600 text-white px-4 py-3 flex justify-between items-center">
-                <div className="text-xl font-bold">MyApp</div>
+            <nav className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 ... text-white px-4 py-3 flex justify-between items-center">
+                <div className="text-xl font-bold">Job Portal</div>
                 <div className="hidden md:flex items-center space-x-6">
                     {
                         navlink.map(data => <NavLink onClick={toggleSidebar} to={data.link}>{data.name}</NavLink>)
                     }
-                    <button className="text-start p-2 rounded bg-black">Login</button>
-                    <button className="text-start p-2 rounded bg-black">LogOut</button>
-                    <button className="text-start rounded p-2 bg-green-600">Register</button>
+                    {
+                        user ? <Link className="text-start p-2 rounded bg-black">LogOut</Link> : <Link className="text-start p-2 rounded bg-black">Login</Link>
+                    }
+                    <Link className="text-start rounded p-2 bg-green-600">Register</Link>
                 </div>
 
                 <div className="md:hidden">
@@ -54,15 +56,16 @@ const Navbar = () => {
             </nav>
 
             {/* Sidebar for Mobile */}
-            <div className={`fixed top-0 left-0 w-64 h-full bg-blue-700 text-white p-5 transform ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out z-50 md:hidden`}>
-                <h2 className="text-2xl font-bold mb-6">Menu</h2>
+            <div className={`fixed  top-0 left-0 w-64 h-full backdrop-blur-lg bg-white/30 rounded-lg text-white p-5 transform ${sidebarOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out z-50 md:hidden`}>
+                <h2 className="text-2xl font-bold mb-6">Job Portal</h2>
                 <nav className="flex flex-col space-y-4">
                     {
                         navlink.map(data => <NavLink onClick={toggleSidebar} to={data.link}>{data.name}</NavLink>)
                     }
-                    <button className="text-start p-2 rounded bg-black">Login</button>
-                    <button className="text-start p-2 rounded bg-black">LogOut</button>
-                    <button className="text-start rounded p-2 bg-green-600">Register</button>
+                    {
+                        user ? <Link  className="text-start p-2 rounded bg-black">LogOut</Link> : <Link className="text-start p-2 rounded bg-black">Login</Link>
+                    }
+                    <Link className="text-start rounded p-2 bg-green-600">Register</Link>
                 </nav>
 
             </div>
