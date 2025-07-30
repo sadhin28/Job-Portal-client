461303b71aa4670a2f2cf1217591bf58280f0fed
sadhin28 <taosifsadhin113@gmail.com>
2025-07-30 12:54:21 +0600

    add view application

--- src/components/Myapplications.jsx
+++ src/components/Myapplications.jsx
@@ -102,7 +102,7 @@ const Myapplications = () => {
                                     </button>
                                     <Link
                                        to={`/viewApplication/${app?.job_id}`}
-                                        className="bg-red-500 hover:bg-red-600 text-white text-sm px-4 py-2 rounded-lg transition duration-200 shadow-md"
+                                        className="bg-green-500 hover:bg-gray-600 text-white text-sm px-4 py-2 rounded-lg transition duration-200 shadow-md"
                                     >
                                         View Application
                                     </Link>
